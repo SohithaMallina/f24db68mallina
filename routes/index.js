94f4cb63bc70122da5539974676059d115eb7e57
SohithaMallina <sohithamallina12@gmail.com>
2024-11-06 19:16:36 -0600

    herbs

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,4 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-const results = [
-  { herb_name: 'Basil', description: 'A fragrant herb used in Italian cuisine', uses: 'Used in cooking, teas, and as garnish' },
-  { herb_name: 'Mint', description: 'A cooling herb commonly used for its medicinal properties', uses: 'Used for digestive problems and skin care' },
-  { herb_name: 'Rosemary', description: 'A woody herb known for its strong aroma', uses: 'Used in roasting meats, flavoring soups, and in aromatherapy' }
-];
-exports.results = results;
+
