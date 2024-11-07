235846b65d433911cff8031c9f712444eb1b2cb1
SohithaMallina <sohithamallina12@gmail.com>
2024-11-06 20:01:44 -0600

    herbs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var herbsRouter = require('./routes/herbs');
+var gridRouter = require('./routes/grid');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/herbs', herbsRouter);
+app.use('/grid', gridRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
