4adc1e437118254b00584d36a0c7c9fe6d4e1a62
SohithaMallina <sohithamallina12@gmail.com>
2024-11-06 19:44:11 -0600

    herbs

--- app.js
+++ app.js
@@ -13,13 +13,11 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/herbs', herbsRouter);
@@ -39,5 +37,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
