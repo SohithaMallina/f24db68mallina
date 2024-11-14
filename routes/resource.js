838f1c8a8a81fae5143d1c6898414cf1bb541067
SohithaMallina <sohithamallina12@gmail.com>
2024-11-13 22:07:00 -0600

    modified

--- routes/resource.js
+++ routes/resource.js
@@ -1,29 +1,28 @@
 var express = require('express');
 var router = express.Router();
+
 // Require controller modules.
 var api_controller = require('../controllers/api');
 var herb_controller = require('../controllers/herb');
-/// API ROUTE ///
+
+// API ROUTE
 // GET resources base.
 router.get('/', api_controller.api);
-/// HERB ROUTES ///
+
+// HERB ROUTES
 // POST request for creating a Herb.
 router.post('/herbs', herb_controller.herb_create_post);
+
 // DELETE request to delete Herb.
 router.delete('/herbs/:id', herb_controller.herb_delete);
+
 // PUT request to update Herb.
 router.put('/herbs/:id', herb_controller.herb_update_put);
+
 // GET request for one Herb.
 router.get('/herbs/:id', herb_controller.herb_detail);
+
 // GET request for list of all Herb items.
 router.get('/herbs', herb_controller.herb_list);
+
 module.exports = router;
-// API for our resources
-exports.api = function(req, res) {
-res.write('[');
-res.write('{"resource":"herbs", ');
-res.write(' "verbs":["GET","PUT", "DELETE"] ');
-res.write('}');
-res.write(']')
-res.send();
-};
