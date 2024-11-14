f5c394e15175495e95e1484613a0c1a1fd6dfd63
SohithaMallina <sohithamallina12@gmail.com>
2024-11-13 22:00:56 -0600

    modified

--- routes/resource.js
+++ routes/resource.js
@@ -2,26 +2,26 @@ var express = require('express');
 var router = express.Router();
 // Require controller modules.
 var api_controller = require('../controllers/api');
-var costume_controller = require('../controllers/costume');
+var herb_controller = require('../controllers/herb');
 /// API ROUTE ///
 // GET resources base.
 router.get('/', api_controller.api);
-/// COSTUME ROUTES ///
-// POST request for creating a Costume.
-router.post('/costumes', costume_controller.costume_create_post);
-// DELETE request to delete Costume.
-router.delete('/costumes/:id', costume_controller.costume_delete);
-// PUT request to update Costume.
-router.put('/costumes/:id', costume_controller.costume_update_put);
-// GET request for one Costume.
-router.get('/costumes/:id', costume_controller.costume_detail);
-// GET request for list of all Costume items.
-router.get('/costumes', costume_controller.costume_list);
+/// HERB ROUTES ///
+// POST request for creating a Herb.
+router.post('/herbs', herb_controller.herb_create_post);
+// DELETE request to delete Herb.
+router.delete('/herbs/:id', herb_controller.herb_delete);
+// PUT request to update Herb.
+router.put('/herbs/:id', herb_controller.herb_update_put);
+// GET request for one Herb.
+router.get('/herbs/:id', herb_controller.herb_detail);
+// GET request for list of all Herb items.
+router.get('/herbs', herb_controller.herb_list);
 module.exports = router;
 // API for our resources
 exports.api = function(req, res) {
 res.write('[');
-res.write('{"resource":"costumes", ');
+res.write('{"resource":"herbs", ');
 res.write(' "verbs":["GET","PUT", "DELETE"] ');
 res.write('}');
 res.write(']')
